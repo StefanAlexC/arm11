1ceb7a587aa4aa836644bab7c113ad2a4e4d09bd
cristina <as9716@ic.ac.uk>
2017-05-26 12:32:17 +0100

    Added initialize and print functions for arm11 structure

--- src/emulate.c
+++ src/emulate.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 struct ARM11 {
     /**
@@ -17,6 +18,54 @@ struct ARM11 {
     uint32_t registers[15];
 };
 
+/**
+ * initializes the memory and the registers of the given arm11 to 0
+ * @param arm11
+ */
+void initialize(struct ARM11 *arm11) {
+  //TODO: replace 65536 and 15 with macros
+  int i;
+  for (i = 0; i < 65536; i++) {
+    arm11->memory[i] = 0;
+  }
+  for (i = 0; i < 15; i++) {
+    arm11->registers[i] = 0;
+  }
+}
+
+/**
+ * prints the registers and the non-zero memory locations of the given arm11
+ * @param arm11
+ */
+void print(struct ARM11 *arm11) {
+  //TODO: replace 65536 and 15 with macros
+  int i;
+  printf("%s\n", "Registers:");
+  for (i = 0; i < 12; i++) {
+    printf("$%-2i :%8i (0x%08x) \n", i, arm11->registers[i], arm11->registers[i]);
+  }
+  printf("PC  :%8i (0x%08x) \n", arm11->registers[13], arm11->registers[13]);
+  printf("CSPR:%8i (0x%08x) \n", arm11->registers[14], arm11->registers[14]);
+
+  printf("%s\n", "Non-zero memory:");
+  for (i = 0; i < 65536; i++) {
+    if (arm11->memory[i] != 0) {
+      printf("%08x:  0x%08x \n", i, arm11->memory[i]);
+    }
+  }
+}
+
 int main(int argc, char **argv) {
+  struct ARM11 arm11;
+  initialize(&arm11);
+
+  /*do {
+     stop = execute(decoded, *arm11);
+     decoded = decode(instr);
+     instr = fetch(*arm11);
+  } while(!stop);
+   */
+
+  print(& arm11);
   return EXIT_SUCCESS;
 }
