45473aecfce91854cc3704458f71bd4184435023
Alex <sc3316@ic.ac.uk>
2017-05-26 17:27:00 +0100

    Tested every method.

--- src/emulate.c
+++ src/emulate.c
@@ -24,18 +24,18 @@ struct ARM11 {
  * @param arm11
  */
 void initialize(struct ARM11 *arm11) {
-  //TODO: replace 65536 and 15 with macros
-  int i;
-  for (i = 0; i < 65536; i++) {
-    arm11->memory[i] = 0;
-  }
-  for (i = 0; i < 15; i++) {
-    arm11->registers[i] = 0;
-  }
+    //TODO: replace 65536 and 15 with macros
+    int i;
+    for (i = 0; i < 65536; i++) {
+        arm11->memory[i] = 0;
+    }
+    for (i = 0; i < 15; i++) {
+        arm11->registers[i] = 0;
+    }
 }
 
 /**
- * X Not Tested X
+ * !!Tested!!
  * Takes a memory address and converts the next 4 bytes following the address from Little Endian to Big Endian
  * and returns the resulting 32-bit int
  * @param i The memory address
@@ -43,13 +43,13 @@ void initialize(struct ARM11 *arm11) {
  * @return The Big Endian number
  */
 uint32_t littleToBig(int i, struct ARM11 *arm11) {
-  uint32_t value = 0;
-  int j;
-  for (j = 0; j < 4; j++) {
-    value <<= 8;
-    value += arm11->memory[i - j];
-  }
-  return value;
+    uint32_t value = 0;
+    int j;
+    for (j = 3; j >= 0; j--) {
+        value <<= 8;
+        value += arm11->memory[i + j];
+    }
+    return value;
 }
 
 /**
@@ -60,13 +60,13 @@ uint32_t littleToBig(int i, struct ARM11 *arm11) {
  * @return value of 32 bit instruction
  */
 uint32_t getMemoryValue(int i, struct ARM11 *arm11) {
-  uint32_t value = 0;
-  int j;
-  for (j = 0; j < 4; j++) {
-    value <<= 8;
-    value += arm11->memory[i + j];
-  }
-  return value;
+    uint32_t value = 0;
+    int j;
+    for (j = 0; j < 4; j++) {
+        value <<= 8;
+        value += arm11->memory[i + j];
+    }
+    return value;
 }
 
 /**
@@ -75,32 +75,33 @@ uint32_t getMemoryValue(int i, struct ARM11 *arm11) {
  * @param arm11
  */
 void print(struct ARM11 *arm11) {
-  //TODO: replace 65536 and 15 with macros
-  int i;
-  printf("%s\n", "Registers:");
-  for (i = 0; i < 12; i++) {
-    printf("$%-2i :%8i (0x%08x) \n", i, arm11->registers[i], arm11->registers[i]);
-  }
-  printf("PC  :%8i (0x%08x) \n", arm11->registers[13], arm11->registers[13]);
-  printf("CSPR:%8i (0x%08x) \n", arm11->registers[14], arm11->registers[14]);
-
-  printf("%s\n", "Non-zero memory:");
-  for (i = 0; i < 65536; i += 4) {
-    uint32_t value = getMemoryValue(i, arm11);
-    if (value != 0) {
-      printf("%08x:  0x%08x \n", i, value);
+    //TODO: replace 65536 and 15 with macros
+    int i;
+    printf("%s\n", "Registers:");
+    for (i = 0; i < 12; i++) {
+        printf("$%-2i :%8i (0x%08x) \n", i, arm11->registers[i], arm11->registers[i]);
     }
-  }
+    printf("PC  :%8i (0x%08x) \n", arm11->registers[13], arm11->registers[13]);
+    printf("CSPR:%8i (0x%08x) \n", arm11->registers[14], arm11->registers[14]);
+
+    printf("%s\n", "Non-zero memory:");
+    for (i = 0; i < 65536; i += 4) {
+        uint32_t value = getMemoryValue(i, arm11);
+        if (value != 0) {
+            printf("%08x:  0x%08x \n", i, value);
+        }
+    }
 }
 
 /**
- * X Not Tested X
+ * !!Tested!!
  * Fetches the next instruction from memory
  * @param arm11
  * @return The fetched 32 bit instruction in Big Endian format
  */
 uint32_t fetch(struct ARM11 *arm11) {
-  return littleToBig(arm11->registers[13], arm11);
+    //TODO: Might need to add error if not multiple of 4
+    return littleToBig(arm11->registers[13], arm11);
 }
 
 /**
@@ -109,16 +110,16 @@ uint32_t fetch(struct ARM11 *arm11) {
  * @param The byte we wish to print
  */
 void printByte_inBinary(uint8_t byte) {
-  uint8_t mask = 0;
+    uint8_t mask = 0;
 
-  for (int i = 7; i >= 0; i--) {
-    mask = (uint8_t) (1 << i);
-    if ((mask & byte) > 0)
-      printf("1");
-    else
-      printf("0");
-  }
-  printf("\n");
+    for (int i = 7; i >= 0; i--) {
+        mask = (uint8_t) (1 << i);
+        if ((mask & byte) > 0)
+            printf("1");
+        else
+            printf("0");
+    }
+    printf("\n");
 }
 
 /**
@@ -127,33 +128,33 @@ void printByte_inBinary(uint8_t byte) {
  * @param fileName The path of the file we wish to read
  */
 void readFile(char *fileName, struct ARM11 *arm11) {
-  FILE *file = fopen(fileName, "r");
-  char byte;
-  int memoryLocation = 0;
+    FILE *file = fopen(fileName, "r");
+    char byte;
+    int memoryLocation = 0;
 
-  while ((byte = (char) fgetc(file)) != EOF) {
-    //printf("%d - ",c);
-    //printByte_inBinary(charToByte(c));
-    arm11->memory[memoryLocation++] = (uint8_t) byte;
-  }
+    while ((byte = (char) fgetc(file)) != EOF) {
+        //printf("%d - ",c);
+        //printByte_inBinary(charToByte(c));
+        arm11->memory[memoryLocation++] = (uint8_t) byte;
+    }
 
-  fclose(file);
+    fclose(file);
 }
 
 
 int main(int argc, char **argv) {
-  struct ARM11 arm11;
-  initialize(&arm11);
-  readFile(argv[1], &arm11);
+    struct ARM11 arm11;
+    initialize(&arm11);
+    readFile(argv[1], &arm11);
 
-  /*do {
-     stop = execute(decoded, *arm11);
-     decoded = decode(instr);
-     instr = fetch(*arm11);
-  } while(!stop);
-   */
+    /*do {
+       stop = execute(decoded, *arm11);
+       decoded = decode(instr);
+       instr = fetch(*arm11);
+    } while(!stop);
+     */
 
-  print(&arm11);
-  return EXIT_SUCCESS;
+    print(&arm11);
+    return EXIT_SUCCESS;
 }
 
