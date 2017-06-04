a984f86e3cc89ec3b755f9154c9efb13b1a1cb34
Kevin Arness <kevin.arness16@imperial.ac.uk>
2017-06-04 19:06:22 +0100

    Fixed address errors

--- src/gpio.s
+++ src/gpio.s
@@ -1,11 +1,11 @@
-ldr r1,=0x00004000
+ldr r1,=0x800
 ldr r0,=0x20200004
 ldr r2,[r0]
 orr r3,r2,r1
 str r3,[r0]
 loop:
 clear:
-ldr r1,=0x00001000
+ldr r1,=0x8000
 ldr r2,=0x20200028
 str r1,[r2]
 mov r0,#0xFF
@@ -13,7 +13,7 @@ sub r0,r0,#1
 cmp r0,#0
 bne clear
 set:
-ldr r1,=0x00001000
+ldr r1,=0x8000
 ldr r2,=0x2020001C
 str r1,[r2]
 mov r0,#0xFF
