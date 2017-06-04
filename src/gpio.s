96b744ac06aa682d808bd5e251146b98c05780c8
ka1816 <Nivek1997>
2017-06-04 16:12:56 +0100

    Assembler code compiles, needs testing

--- src/gpio.s
+++ src/gpio.s
@@ -1,20 +1,24 @@
-mov r1,#0x00004000
-orr r1,[#0x20200004]
-str r1,[#0x20200004]
+ldr r1,=0x00004000
+ldr r0,=0x20200004
+ldr r2,[r0]
+orr r3,r2,r1
+str r3,[r0]
 loop:
 clear:
-mov r1,#0x00001000
-str r1,[#0x20200028]
+ldr r1,=0x00001000
+ldr r2,=0x20200028
+str r1,[r2]
 mov r0,#0xFF
-sub r0,#00000001
-cmp r0,#0x0
+sub r0,r0,#1
+cmp r0,#0
 bne clear
 set:
-mov r1,#0x00001000
-str r1,[#0x2020001C]
+ldr r1,=0x00001000
+ldr r2,=0x2020001C
+str r1,[r2]
 mov r0,#0xFF
-sub r0,#00000001
-cmp r0,#0x0
+sub r0,r0,#1
+cmp r0,#0
 bne set
 b loop
 
