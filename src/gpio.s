e6f791cd9b651592188fe75d2519b2d9091c2f02
ka1816 <Nivek1997>
2017-06-04 22:17:41 +0100

    Fixing bug in code, LED is accessed and turns on but does not switch off

--- src/gpio.s
+++ src/gpio.s
@@ -1,24 +1,25 @@
-ldr r1,=0x800
 ldr r0,=0x20200004
 ldr r2,[r0]
-orr r3,r2,r1
+orr r3,r2,#0x100000
 str r3,[r0]
+mov r4,#25
 loop:
-clear:
-ldr r1,=0x8000
+ldr r1,=0x10000
 ldr r2,=0x20200028
 str r1,[r2]
-mov r0,#0xFF
+ldr r0,=10
+clear:
 sub r0,r0,#1
 cmp r0,#0
 bne clear
-set:
-ldr r1,=0x8000
+ldr r1,=0x10000
 ldr r2,=0x2020001C
 str r1,[r2]
-mov r0,#0xFF
+ldr r0,=10
+set:
 sub r0,r0,#1
 cmp r0,#0
 bne set
-b loop
-
+sub r4,r4,#1
+cmp r4,#0
+bne loop
